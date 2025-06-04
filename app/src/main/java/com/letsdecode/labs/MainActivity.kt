89d69f70c6d3b30eecd640c14961ad20419a3727
Gaurav Bajaj <letsdecode@gmail.com>
2025-06-04 12:18:39 -0700

    adds button feature to the main activity

--- app/src/main/java/com/letsdecode/labs/MainActivity.kt
+++ app/src/main/java/com/letsdecode/labs/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.letsdecode.labs.ui.theme.LabsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +24,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             LabsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Apply the padding globally to the whole BottomBar
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        // Use the padding inside the Column to apply it to the Text composable
+                        FeatureFlow(
+                            "Traditional UI",
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxWidth(),
+                            onClick = { println("Column clicked!") })
+                    }
                 }
             }
         }
@@ -31,17 +41,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FeatureFlow(
+    name: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    onClick: () -> Unit = { -> }
+) {
+    Button(modifier = modifier, onClick = onClick) {
+        Text(
+            text = name,
+            color = color,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LabsTheme {
-        Greeting("Android")
+        FeatureFlow("Android")
     }
 }
